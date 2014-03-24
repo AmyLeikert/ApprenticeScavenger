63e8ef47abee30e738c9fd9144b7240edee90b43
Amy Leikert <amy.leikert@detroitlabs.com>
2014-03-24 14:14:29 -0400

    first commit

---
+++ DLScavengerTests/DLScavengerTests.m
@@ -0,0 +1,34 @@
+//
+//  DLScavengerTests.m
+//  DLScavengerTests
+//
+//  Created by DetroitLabsUser on 3/24/14.
+//  Copyright (c) 2014 DetroitLabsUser. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DLScavengerTests : XCTestCase
+
+@end
+
+@implementation DLScavengerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
