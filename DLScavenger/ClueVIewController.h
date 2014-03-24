55462ad9a6bb370ea67a3c1ae5948242a1f2c7d8
Mechelle Sieglitz <mechelle.sieglitz@detroitlabs.com>
2014-03-24 14:28:52 -0400

    Added ClueViewController

---
+++ DLScavenger/ClueVIewController.h
@@ -0,0 +1,13 @@
+//
+//  ClueVIewController.h
+//  DLScavenger
+//
+//  Created by Detroit Labs User on 3/24/14.
+//  Copyright (c) 2014 DetroitLabsUser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ClueVIewController : UIViewController
+
+@end
---
+++ DLScavenger/ClueVIewController.m
@@ -0,0 +1,38 @@
+//
+//  ClueVIewController.m
+//  DLScavenger
+//
+//  Created by Detroit Labs User on 3/24/14.
+//  Copyright (c) 2014 DetroitLabsUser. All rights reserved.
+//
+
+#import "ClueVIewController.h"
+
+@interface ClueVIewController ()
+
+@end
+
+@implementation ClueVIewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
