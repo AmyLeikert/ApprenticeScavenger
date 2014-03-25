a863689b72575ecdb6e6c08d7131c823d3d4ffb9
Eliot Williams <eliot.williams@detroitlabs.com>
2014-03-25 16:04:15 -0400

    Added Clue View, slight changes to Hunt View. Added DLHunt class

---
+++ DLScavenger/DLClueViewController.h
@@ -0,0 +1,13 @@
+//
+//  DLClueViewController.h
+//  DLScavenger
+//
+//  Created by DetroitLabsUser on 3/25/14.
+//  Copyright (c) 2014 DetroitLabsUser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DLClueViewController : UIViewController
+
+@end
